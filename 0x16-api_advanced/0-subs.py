c782bd34ded2f3ca8e76016059a40fc2e07cf251
bizzy604 <kevinyamatta50@gmail.com>
2024-08-13 08:07:47 -0700

    Automated commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,32 @@
-import requests
+#!/usr/bin/python3
+"""
+number of subscribers for a given subreddit
+"""
+
+from requests import get
+
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'MyRedditApp/0.1'}
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        # Check if the status code is 200 (OK) and Content-Type is JSON
-        if response.status_code == 200 and response.headers['Content-Type'] == 'application/json; charset=utf-8':
-            data = response.json()
-            return data['data']['subscribers']
-        
-        # If not successful or not JSON, return 0
+    """
+    Function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    except Exception as e:
-        # If any other error occurs, return 0
+    user_agent = {'User-agent': 'MyRedditApp/0.1'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    
+    try:
+        response = get(url, headers=user_agent, allow_redirects=False)
+
+        if response.status_code == 200 and response.headers.get('Content-Type') == 'application/json; charset=utf-8':
+            results = response.json()
+            return results.get('data', {}).get('subscribers', 0)
+        else:
+            return 0
+
+    except Exception:
         return 0
 
